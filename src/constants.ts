d952747749b626b5abc09e545042593852e9a4bb
xxx <humengmingxx@gmail.com>
2022-05-21 14:58:13 +0800

    feat(re)：up

--- src/constants.ts
+++ src/constants.ts
@@ -3,4 +3,4 @@
  *   暂时只支持 {intl.formatMessage({ id: 'xxx' })}
  *   等完成最小闭环后补全
  */
-export const INTL_RE = /(?<=formatMessage\(\s?\{\s?id:\s?)['"].*?['"]/gmi
+export const INTL_RE = /(?<=\bformatMessage\(\s*\{\s*id:\s*['"`]).*?(?=['"`])/gmi
