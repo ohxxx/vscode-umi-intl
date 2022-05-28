503712af682058d57ddc44f7f71c85bedc59d662
xxx <humengmingxx@gmail.com>
2022-05-28 23:19:17 +0800

    feat(playground)：up

--- playground/umi/src/locales/zhCN/title.ts
+++ playground/umi/src/locales/zhCN/title.ts
@@ -1,3 +1,17 @@
 export const title = {
   title: '我是一个小标题',
 }
+
+export const head = {
+  head: '我是 head',
+}
+
+export const tail = {
+  tail: '我是 tail',
+}
+
+const xxx = {
+  xxx: 'i am xxx',
+}
+
+export default xxx
